c56ae5c42b283735456e4d2a49990d9e85f1d6b6
TravisAlstrand <travisalstrand@gmail.com>
2022-02-25 10:21:21 -0800

    Commit 6
    No repeat quotes / colors functionality added.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,8 @@ project 1 - A Random Quote Generator
 
 const body = document.getElementById('body');
 const quoteBox = document.getElementById('quote-box');
-let currentQuote;
+
+// ==================================================================================
 
 /*** 
  * `quotes` array 
@@ -63,6 +64,11 @@ const quotes = [
   }
 ];
 
+// empty array to be changed over time in getRandomQuote()
+let newQuotesArr = [];
+
+// ==================================================================================
+
 /*** 
  * `colors` array 
 ***/
@@ -72,84 +78,115 @@ const colors = [
   "#c13a3a", "#c13a8d", "#963ac1", "#3a3cc1", "#3ab8c1"
 ];
 
+// empty array to be changed over time in getRandomColor()
+let newColorsArr = [];
+
+// ==================================================================================
+
 /***
  * `getRandomColor` function
 ***/
 
 function getRandomColor() {
-  // get a random number between 0 & the last number of colors array  
-  const randomNumber = Math.floor(Math.random() * colors.length);
-  // return the color in colors array at random number index
-  return colors[randomNumber];
+
+  // check if newColorsArr is empty
+  if (newColorsArr.length === 0) {
+
+    // if so, duplicate colors array
+    newColorsArr = [...colors];
+  }
+  
+  // get a random number between 0 & the last number of newColorsArr
+  const randomNumber = Math.floor(Math.random() * newColorsArr.length);
+
+  // return the color in newColorsArr at random number index
+  return newColorsArr[randomNumber];
 }
+
+// ==================================================================================
 
 /***
  * `getRandomQuote` function
 ***/
 
 function getRandomQuote() {
-  // get a random number between 0 & the last number of quotes array
-  const randomNumber = Math.floor(Math.random() * quotes.length);
-  // return the object in quotes array at random number index
-  return quotes[randomNumber];
+
+  // check if newQuotesArr is empty
+  if (newQuotesArr.length === 0) {
+
+    // if so, duplicate quotes array
+    newQuotesArr = [...quotes];
+  }
+
+  // get a random number between 0 & the last number of newQuotesArr
+  const randomNumber = Math.floor(Math.random() * newQuotesArr.length);
+
+  // return the object in newQuotesArr at random number index
+  return newQuotesArr[randomNumber];
 }
+
+// ==================================================================================
 
 /***
  * `printQuote` function
 ***/
 
 function printQuote() {
-  // call getRandomQuote and assign to 'nextQuote' variable
-  let nextQuote = getRandomQuote();
+
+  // call getRandomQuote and assign to 'newQuote' variable
+  const newQuote = getRandomQuote();
+
   // call getRandomColor and assign to 'newColor' variable
-  let newColor = getRandomColor();
+  const newColor = getRandomColor();
 
-  // check if newly acquired quote object is the same as the last (currentQuote)
-  if (nextQuote === currentQuote) {
-    // if so, call getRandomQuote again and re assign 'nextQuote' variable
-    nextQuote = getRandomQuote();
-  } else {
-    // if not, assign new quote to 'current quote' variable
-    currentQuote = nextQuote;
+  // variable for html replacement code
+  let newHTML = `
+    <p class="quote">${newQuote.quote}</p>
+    <p class="source">${newQuote.source}
+  `;
 
-    // variable for html replacement code
-    let newHTML = `
-      <p class="quote">${currentQuote.quote}</p>
-      <p class="source">${currentQuote.source}
-    `;
-
-    // check if there is a citation value
-    if (currentQuote.citation !== null) {
-      newHTML += `<span class="citation">${currentQuote.citation}</span>`;
-    }
-
-    // check if there is a year value
-    if (currentQuote.year !== null) {
-      newHTML += `<span class="year">${currentQuote.year}</span>`
-    }
-
-    // check if there is a tags value
-    if (currentQuote.tags !== null) {
-      newHTML +=  `</p><p class="tags">${currentQuote.tags}`
-    }
-
-    // complete html markup
-    newHTML += `</p>`;
-
-    // assign html markup to DOM
-    quoteBox.innerHTML = newHTML;
-
-    // assign new background color
-    body.style.backgroundColor = newColor;
+  // check if there is a citation value
+  if (newQuote.citation !== null) {
+    newHTML += `<span class="citation">${newQuote.citation}</span>`;
   }
+
+  // check if there is a year value
+  if (newQuote.year !== null) {
+    newHTML += `<span class="year">${newQuote.year}</span>`
+  }
+
+  // check if there is a tags value
+  if (newQuote.tags !== null) {
+    newHTML +=  `</p><p class="tags">${newQuote.tags}`
+  }
+
+  // complete html markup
+  newHTML += `</p>`;
+
+  // assign html markup to DOM
+  quoteBox.innerHTML = newHTML;
+
+  // assign new bg color
+  body.style.backgroundColor = newColor;
+
+  // remove newQuote from newQuotesArr
+  newQuotesArr.splice(newQuotesArr.indexOf(newQuote), 1);
+
+  // remove new bg color from newColorsArr
+  newColorsArr.splice(newColorsArr.indexOf(newColor), 1);
 }
 
+// ==================================================================================
+
 printQuote();
+
+// ==================================================================================
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
+
 // for button
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
 
